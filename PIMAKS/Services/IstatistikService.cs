c3f17fbcfc171f68e22a01e03cde66a1a561be1c
barisgevher <nostradamiz5@gmail.com>
2025-08-22 01:18:48 +0300

    feat: minor changes and start point to versioning

--- PIMAKS/Services/IstatistikService.cs
+++ PIMAKS/Services/IstatistikService.cs
@@ -19,13 +19,13 @@ namespace PIMAKS.Services
 
         public async Task<MakineIstatistikResponseDto> GetMakineIstatistikAsync(MakineIstatistikRequestDto request)
         {
-            // Belirtilen makinenin, verilen tarih aralığıyla çakışan tüm kiralamalarını bul.
+            
             var ilgiliKiralamalar = await _context.Kiralamas
                 .Where(k => k.MakineId == request.MakineId &&
                             k.BaslangicTarihi <= request.BitisTarihi &&
                             k.BitisTarihi >= request.BaslangicTarihi)
-                .Include(k => k.Makine) // Makinenin birim fiyatını almak için
-                .Include(k => k.Nakliye) // Nakliye ücretini almak için
+                .Include(k => k.Makine) 
+                .Include(k => k.Nakliye) 
                 .ToListAsync();
 
             int toplamGun = 0;
@@ -33,7 +33,7 @@ namespace PIMAKS.Services
 
             foreach (var kiralama in ilgiliKiralamalar)
             {
-                // Her kiralama için, istenen tarih aralığı içindeki günleri hesapla
+                
                 var baslangic = kiralama.BaslangicTarihi > request.BaslangicTarihi ? kiralama.BaslangicTarihi : request.BaslangicTarihi;
                 var bitis = kiralama.BitisTarihi < request.BitisTarihi ? kiralama.BitisTarihi : request.BitisTarihi;
 
@@ -41,7 +41,7 @@ namespace PIMAKS.Services
 
                 toplamGun += gunSayisi;
 
-                // Getiriyi hesapla: (gün * birim fiyat) + nakliye ücreti
+                
                 decimal kiralamaGetirisi = (decimal)gunSayisi * (decimal)kiralama.Makine.BirimFiyat;
                 decimal nakliyeUcreti = kiralama.Nakliye != null ? kiralama.Nakliye.NakliyeUcreti : 0;
 
@@ -57,18 +57,16 @@ namespace PIMAKS.Services
         }
         public async Task<IEnumerable<MakineGenelIstatistikDto>> GetAllMakineIstatistikAsync()
         {
-            // Adım 1: Gerekli tüm veriyi, karmaşık hesaplama yapmadan, veritabanından çek.
-            // Bu sorgu, EF'in kolayca SQL'e çevirebileceği basit bir sorgudur.
             var makinelerVeKiralamalari = await _context.Makines
                 .Include(m => m.Marka)
                 .Include(m => m.Kiralamas)
                     .ThenInclude(k => k.Nakliye)
                 .Select(m => new
                 {
-                    // DTO için ihtiyacımız olan alanları seçiyoruz
+                   
                     m.MakineId,
                     m.MakineKodu,
-                    MarkaAdi = m.Marka != null ? m.Marka.Marka1 : "Belirtilmemiş", // Null kontrolü
+                    MarkaAdi = m.Marka != null ? m.Marka.Marka1 : "Belirtilmemiş", 
                     m.BirimFiyat,
                     Kiralamalari = m.Kiralamas.Select(k => new
                     {
@@ -79,7 +77,7 @@ namespace PIMAKS.Services
                 })
                 .ToListAsync();
 
-            // Adım 2: Çektiğimiz bu veri üzerinde, C# içinde (bellekte) hesaplamaları yap.
+            
             var sonuclar = makinelerVeKiralamalari.Select(m => new MakineGenelIstatistikDto
             {
                 MakineId = m.MakineId,
@@ -88,7 +86,7 @@ namespace PIMAKS.Services
                 ToplamKiralamaSayisi = m.Kiralamalari.Count(),
                 ToplamGetiri = m.Kiralamalari.Sum(k =>
                 {
-                    // Tarih farkını burada, C# içinde güvenle hesaplayabiliriz.
+                   
                     var gunSayisi = (k.BitisTarihi - k.BaslangicTarihi).TotalDays + 1;
                     if (gunSayisi < 1) gunSayisi = 1;
 
